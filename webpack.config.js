bbf105896cec863794b3164ee769f70b4d800eef
huanshenyi <txy1226052>
2020-07-30 21:21:20 +0900

    htmlPlugin&&cleanwebpackPlugin追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry : "./src/main.js",
@@ -14,5 +16,11 @@ module.exports = {
          }
        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
     mode: "development"
 }
